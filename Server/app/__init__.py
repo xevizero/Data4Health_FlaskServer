7f5659589d8fc204088b26b0a57c327d0b0259fb
ledene <lodi.teo@gmail.com>
2018-11-30 11:58:49 +0100

    Aggiunta db nuovo da zero

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-#from flask_json import FlaskJSON , JsonError , json_response, as_json,jsonify
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,6 +10,5 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
-#FlaskJSON(app)
 
 from app import routes, models
