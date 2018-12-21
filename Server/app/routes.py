79baf6af6241c0accdbb074284a15f2ae1d42ffb
Jacopo Frasson <jacopo.frasson.95@gmail.com>
2018-12-21 13:40:35 +0100

    Fix

--- Server/app/routes.py
+++ Server/app/routes.py
@@ -133,6 +133,8 @@ def data4helpapi():
         for id in weightToIntIDs:
             if id in legalIntIDs:
                 legalIntIDs.remove(id)
+    res = None
+    stringsql = None
     if argument == 'BloodPressure':
         #result = BloodPressure.query.with_entities(BloodPressure.bloodPressureUserId,
         #                                           BloodPressure.bloodPressureLowValue,
